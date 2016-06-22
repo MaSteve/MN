ac93ed7e975b115f6a978ce784a2108d02c549f6
Marco Antonio Garrido Rojo <3d17marcoantonio.garrido@gmail.com>
2016-06-22 18:45:53 +0200

    Evita permutaciones

--- gauss.m
+++ gauss.m
@@ -8,17 +8,17 @@ function [sol, gaussmat, punt] = gaussM(A, b) %Eliminación gaussiana
     punt = 1:n;
 
     for i = 1:n-1
-        [m pos] = max(abs(A(i:n, i)));
+        [m pos] = max(abs(A(punt(i:n), i)));
 
-        A([i, pos+i-1], :) = A([pos+i-1, i], :);
+        %A([i, pos+i-1], :) = A([pos+i-1, i], :); %no permutar
         punt([i, pos+i-1]) = punt([pos+i-1, i]);
 
         for j = i+1:n
-            A(j, i) = (A(j, i)/A(i, i));
+            A(punt(j), i) = (A(punt(j), i)/A(punt(i), i));
         end
 
         for j = i+1:n
-            A(j, i+1:n) = A(j, i+1:n) - (A(i, i+1:n)*A(j, i));
+            A(punt(j), i+1:n) = A(punt(j), i+1:n) - (A(punt(i), i+1:n)*A(punt(j), i));
         end
     end
 
@@ -33,14 +33,14 @@ function sol = gausssolver(A, b, punt)
     w(1) = b(punt(1));
 
     for i = 2:n
-        w(i) = b(punt(i)) - dot(A(i, 1:i-1), w(1:i-1));
+        w(i) = b(punt(i)) - dot(A(punt(i), 1:i-1), w(1:i-1));
     end
 
     sol = zeros(n,1);
 
-    sol(n) = w(n)/A(n, n);
+    sol(n) = w(n)/A(punt(n), n);
 
     for i = n-1:-1:1
-        sol(i) = (w(i)-dot(A(i, i+1:n), sol(i+1:n)))/A(i, i);
+        sol(i) = (w(i)-dot(A(punt(i), i+1:n), sol(i+1:n)))/A(punt(i), i);
     end
 end
